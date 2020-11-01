c3cdc739acd9f4558d2df7f270ec22329a3ab35d
nevalzeynep <nevalzynp@gmail.com>
2020-11-01 14:33:01 +0300

    .

--- yetenekkapisi/src/test/java/Base/BaseTests.java
+++ yetenekkapisi/src/test/java/Base/BaseTests.java
@@ -2,6 +2,7 @@ package Base;
 
 import java.util.concurrent.TimeUnit;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -44,5 +45,7 @@ public class BaseTests {
 		Thread.sleep(2000);
 		
 	}
+	
+	
 
 }
--- yetenekkapisi/src/test/java/EdtiProfile/EditProfilTest.java
+++ yetenekkapisi/src/test/java/EdtiProfile/EditProfilTest.java
@@ -15,13 +15,14 @@ public class EditProfilTest extends BaseTests {
 		LoginPage.setPassword("Y3K_StU3nt.462A.!");
 		StudentDashboard stdDashboard=LoginPage.clickStudentLogin();
 	    EditProfilePage editprofile=stdDashboard.clickEditProfileButton();
-	   
+	    
 		editprofile.SetName("batuhan deneme");
 		editprofile.SetSurname("ekici deneme");
 		editprofile.setTckn("41546586464");
 		editprofile.setGender(0,"male");
 		editprofile.setMilitaryServiceStatus(1);
-		editprofile.setBirthDate();
+		editprofile.setBirthDate("02/02/1997");
+		editprofile.setPhoneNumber("555555555");
 		editprofile.setnationality(6);
 		editprofile.setAddress("çankaya yýldýz 4/2");
 		editprofile.setCountry(0);
